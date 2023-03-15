54bcf7086161e938ee938ac24d8b77cc42f08370
Valeriya Kashirina <Aronilo@mail.ru>
2023-03-16 00:13:07 +0300

    1234

--- lb_3.js
+++ lb_3.js
@@ -7,3 +7,34 @@ open_modal.addEventListener('click', function() {
     modal_window.classList.toggle('display-nan');
 });
 
+const show = formContent.querySelector('.show')
+const inputPassword = formContent.querySelector('.password')
+const inputEmail = formContent.querySelector('.email')
+
+const passwordText = formContent.querySelector('.error-password')
+const emailText = formContent.querySelector('.error-email')
+
+formClose.addEventListener('click', function() {
+    modal.classList.remove('modal-active')
+})
+
+formContent.addEventListener('click', (event) => {
+    console.log("form content");
+    event.preventDefault()
+    
+    if (event.target.classList.contains('exit')) {
+        console.log('exit');
+        modal.classList.add('hidden')
+    }
+
+    show.addEventListener('mousedown', (event) => {
+        event.preventDefault()
+        inputPassword.setAttribute('type','text')
+    })
+
+    show.addEventListener('mouseup', (event) => {
+        event.preventDefault()
+        inputPassword.setAttribute('type','password')
+    })
+
+})
