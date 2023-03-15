4f5a9b9a686c4062d09b3fea12ad8fd4cd9e8fd5
Aronilo <Aronilo@mail.ru>
2023-03-16 01:36:07 +0300

    new 3 lab

--- lb_3.js
+++ lb_3.js
@@ -1,40 +1,75 @@
 const modal_window = document.querySelector('.modal-window');
 const open_modal = document.querySelector('.open-modal');
 
-console.log(open_modal);
-open_modal.addEventListener('click', function() {
-    console.log('click');
-    modal_window.classList.toggle('display-nan');
-});
-
+const formContent = modal_window.querySelector('.form')
 const show = formContent.querySelector('.show')
-const inputPassword = formContent.querySelector('.password')
-const inputEmail = formContent.querySelector('.email')
+const inputPassword = formContent.querySelector('#password')
+const inputEmail = formContent.querySelector('#email')
+const exitButton = formContent.querySelector(`.exit`)
 
 const passwordText = formContent.querySelector('.error-password')
 const emailText = formContent.querySelector('.error-email')
 
-formClose.addEventListener('click', function() {
-    modal.classList.remove('modal-active')
+
+open_modal.addEventListener('click', function(event) {
+    modal_window.classList.toggle('display-nan')
 })
 
-formContent.addEventListener('click', (event) => {
-    console.log("form content");
-    event.preventDefault()
-    
-    if (event.target.classList.contains('exit')) {
-        console.log('exit');
-        modal.classList.add('hidden')
+exitButton.addEventListener('click', function(event) {
+    modal_window.classList.toggle('display-nan')
+})
+
+formContent.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    const registerForm = event.currentTarget;
+    const formData = new FormData(registerForm);
+
+    console.table({
+        "Пароль": formData.get("user-password"),
+        "Email": formData.get("user-email")
+    });
+})
+
+modal_window.addEventListener('click', (event) =>{
+    if (event.target === event.currentTarget) {
+        modal_window.classList.add(`display-nan`)
+    }
+})
+
+show.addEventListener('pointerdown', (event) => {
+    inputPassword.setAttribute('type','text')
+})
+
+show.addEventListener('pointerup', (event) => {
+    inputPassword.setAttribute('type','password')
+})
+
+function vailed(event) {
+    const input = event.currentTarget;
+    const validity = input.validity;
+    let errorMessage = "";
+    if(validity.valueMissing) {
+        errorMessage = "Поле не заполнено";
+    }
+    else if (validity.badInput) {
+        errorMessage = "Поле заполнено неверно";
+    }
+    else if (validity.tooShort) {
+        errorMessage = `Минимальная длина ${input.minLength} сим.`;
+    }
+    else if (validity.typeMismatch) {
+        errorMessage = "Неправильный формат ввода";
+    }
+    else if (validity.valid) {
+        errorMessage = "";
     }
 
-    show.addEventListener('mousedown', (event) => {
-        event.preventDefault()
-        inputPassword.setAttribute('type','text')
-    })
+    input.setCustomValidity(errorMessage);
+    const formRow = input.closest(`.form__row`);
+    const error = formRow.querySelector(`.error`);
+    error.textContent = errorMessage;
+}
 
-    show.addEventListener('mouseup', (event) => {
-        event.preventDefault()
-        inputPassword.setAttribute('type','password')
-    })
-
-})
+inputPassword.addEventListener('blur', vailed);
+inputEmail.addEventListener('blur', vailed);
