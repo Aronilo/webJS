0f09d242f726fa956ec4889ced6f0d7711522789
Valeriya Kashirina <Aronilo@mail.ru>
2023-05-24 22:06:12 +0300

    yeeeeeeeeey

--- lb8/node-sapr-mongodb/index.js
+++ lb8/node-sapr-mongodb/index.js
@@ -1,18 +1,50 @@
 const express = require("express");
 const morgan = require("morgan");
+const swaggerUi = require("swagger-ui-express");
+const swaggerJsDoc = require("swagger-jsdoc");
 
-const commentsController = require("./controllers/comments");
+const controller = require("./controllers/controller");
+const swaggerJSON = require("./docs.json");
+
 const app = express();
 const PORT = 3000;
 
+
+
+
+app.use(express.json());
+
 app.use(morgan('dev'));
+
+app.use(
+  "/api-docs",
+  swaggerUi.serve,
+  swaggerUi.setup(swaggerJSON)
+);
 
 app
   .route("/comments")
-  .get(commentsController.getComments)
-  .post(express.json(), commentsController.postComments);
+  .get(controller.getComments)
+  .post(express.json(), controller.postComments);
 
-app.get("/comments/:id", commentsController.getComment);
+app
+  .route("/models")
+  .get(controller.getModels)
+  .post(controller.check, controller.postModels)
+
+
+app
+  .route("/models/:id")
+  .get(controller.getModel)
+  .put(controller.check, controller.putModel)
+  .delete(controller.check, controller.deleteModel)
+
+
+
+
+app.get("/comments/:id", controller.getComment);
+
+app.use(controller.errorsController);
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
---
+++ lb8/node-sapr-mongodb/services/service.js
@@ -0,0 +1,122 @@
+const { ObjectId } = require("mongodb");
+const connectToMongoDB = require("../configs/mongodb.config");
+
+let db;
+
+connectToMongoDB()
+  .then((result) => {
+    db = result;
+  })
+  .catch((err) => console.log(err));
+
+console.log(db);
+
+async function insertComment(data) {
+  const comments = db.collection("comments");
+  await comments.insertOne(data);
+}
+
+async function findComments() {
+  const comments = db.collection("comments");
+  const result = await comments.find();
+  return result.toArray();
+}
+
+async function findComment(id) {
+  const comments = db.collection("comments");
+  const result = await comments.findOne({ _id: new ObjectId(id) });
+  return result;
+}
+
+async function findModels(collectionName, query, projection) {
+  try {
+    const collection = db.collection(collectionName);
+    const result = await collection.find(query).project(projection).toArray();
+    return result;
+  } catch (err) {
+    return null;
+  }
+}
+
+async function findModel(collectionName, query) {
+  try {
+    console.log("query:", query);
+    const collection = db.collection(collectionName);
+    const result = await collection.findOne({ _id: new ObjectId(query) });
+    return result;
+  } catch (err) {
+    return null;
+  }
+}
+
+async function updateModel(collectionName, id, document) {
+  try {
+    console.log("updateModel", id, document);
+    const collection = db.collection(collectionName);
+    const model = await collection.findOne({ _id: new ObjectId(id) });
+    console.log("model:", model);
+    if (model) {
+      document.time_update = new Date();
+      const result = await collection.updateOne(
+        { _id: new ObjectId(id) },
+        { $set: document }
+      );
+      console.log("result:", result);
+      return result;
+    } else {
+      return null;
+    }
+  } catch (err) {
+    return null;
+  }
+}
+
+async function removeModel(collectionName, id) {
+  try {
+    console.log("deleteModel", id);
+    const collection = db.collection(collectionName);
+    const result = await collection.deleteOne({ _id: new ObjectId(id) });
+    if (result.deletedCount == 0) return null;
+    console.log("result:", result);
+    return result;
+  } catch (err) {
+    return null;
+  }
+}
+
+async function insertModel(collectionName, document) {
+  try {
+    const collection = db.collection(collectionName);
+      document.time_create = new Date();
+      document.time_update = new Date();
+    return await collection.insertOne(document);
+  } catch (err) {
+    return err;
+  }
+}
+
+async function checkApiKey(collectionName, query) {
+  try {
+    const collection = db.collection(collectionName);
+    console.log("query:", query);
+    let result = null;
+    result = await collection.findOne({ api_key: Number(query) });
+    console.log("result:", result);
+    return result;
+  } catch (err) {
+    console.log("ERROR:", err);
+    return err;
+  }
+}
+
+module.exports = {
+  insertComment,
+  findComments,
+  findComment,
+  findModels,
+  findModel,
+  updateModel,
+  removeModel,
+  insertModel,
+  checkApiKey,
+};
